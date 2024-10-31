e0701b4021ff48a5ee1424ecd1015ab520d3a4ef
Ian Manlupig <imanlupig@academic.rrc.ca>
2024-10-31 04:45:21 -0500

    created seeds for cards and owners

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,36 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+require 'httparty'
+require 'faker'
+
+#fetch data from YuGiOh API
+API_URL = 'https://db.ygoprodeck.com/api/v7/cardinfo.php'
+response = HTTParty.get(API_URL)
+cards_data = JSON.parse(response.body)["data"]
+
+cards_data.each do |card_data|
+  #find or create CardType
+  card_type = Category.find_or_create_by(name: card_data["type"])
+
+  #Create card with associated CardType
+  card = Card.create(
+    name: card_data["name"],
+    desc: card_data["desc"],
+    atk: card_data["atk"],
+    def: card_data["def"],
+    card_type: card_type
+  )
+  puts "Creating Card: #{card.name}"
+  if card.persisted?
+    puts "Created: #{card.name}"
+  else
+    puts "Failed to make card: #{card.errors.full_messages.join(", ")}"
+  end
+end
+
+15.times do
+  owner = CardOwner.create(name: Faker::Name.name,)
+
+  owner.cards << Card.order("RANDOM()").limit(10)
+end
