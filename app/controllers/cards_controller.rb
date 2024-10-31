d9a1304201a208d8f2b9b55d49c0ac891732bda8
Ian Manlupig <imanlupig@academic.rrc.ca>
2024-10-31 10:00:05 -0500

    updated cards page to have card type filter

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,16 +1,27 @@
 class CardsController < ApplicationController
   def index
-    if params[:query].present?
-      @results = Card.where("name LIKE ?", "%#{params[:query]}%")
+    if params[:query].present? || params[:card_type_id].present?
+      # show all cards
+      @results = Card.all
+
+      if params[:query].present?
+        @results = @results.where("name LIKE ?", "%#{params[:query]}%")
+      end
+
+      if params[:card_type_id].present?
+        @results = @results.where(card_type_id: params[:card_type_id])
+      end
+
       @cards = []
     else
+
       @cards = Card.limit(200)
       @results = []
     end
   end
 
   def search
-    redirect_to cards_path(query: params[:query])
+    redirect_to cards_path(query: params[:query], card_type_id: params[:card_type_id])
   end
 
   def show
