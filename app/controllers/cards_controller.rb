88f3f98a3e37c6dbe99d45f0ae2c3e5b94d83202
Ian Manlupig <imanlupig@academic.rrc.ca>
2024-10-31 10:15:23 -0500

    added pagination to card page using kaminari

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -4,18 +4,21 @@ class CardsController < ApplicationController
       # show all cards
       @results = Card.all
 
+      # apply name search
       if params[:query].present?
         @results = @results.where("name LIKE ?", "%#{params[:query]}%")
       end
 
+      # apply type filter
       if params[:card_type_id].present?
         @results = @results.where(card_type_id: params[:card_type_id])
       end
 
+      @results = @results.page(params[:page]).per(25)
       @cards = []
     else
 
-      @cards = Card.limit(200)
+      @cards = Card.limit(200).page(params[:page]).per(25)
       @results = []
     end
   end
